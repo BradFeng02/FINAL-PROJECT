2d1760010c588c31ab77b608d40514cef3683231
Brad <bradfeng02@gmail.com>
2021-11-23 22:43:48 -0500

    added description to train.py

--- train.py
+++ train.py
@@ -1,5 +1,13 @@
+"""
+trains svc and serializes fitted model
+using polarity dataset v2.0 at https://www.cs.cornell.edu/people/pabo/movie-review-data/
+"""
+
+from joblib import dump
 import numpy as np
 from sklearn import svm
+
+CLF_PATH = 'model.clf'
 
 N_SAMPLES = 1000  # 1k pos, 1k neg
 POS = 1
@@ -20,6 +28,8 @@ samples = np.zeros((2 * N_SAMPLES, N_FEAT), dtype=np.float64)
 labels = np.zeros(2 * N_SAMPLES, dtype=np.float64)
 labels[N_SAMPLES:] = 1  # 0-999 neg, 1000-1999 pos
 
-
+# TODO features
 
 clf.fit(samples, labels)
+
+dump(clf, CLF_PATH)
